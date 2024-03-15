8c390e0ee38189c03f3dbf8cce8ae10e59236c13
NEMS Linux <nems@category5.tv>
2024-03-15 17:23:55 -0400

    Disable fortune for now - no flush()

--- debpack/var/www/html/power/commands/functions.php
+++ debpack/var/www/html/power/commands/functions.php
@@ -3,16 +3,18 @@ function waitForNEMS() {
   $updating = intval(trim(shell_exec('/usr/local/bin/nems-info quickfix')));
   if ($updating == 1) {
     $iteration = 0;
-    echo 'Waiting for NEMS Linux to finish updating. Please wait.<br />';
+    echo 'Waiting for NEMS Linux to finish updating. Please wait...<br />';
     flush();
     while ($updating == 1) {
       sleep(5);
+/*
       if ($iteration++ == 5) {
         $fortune = shell_exec('/usr/local/share/nems-fortune/fortune.sh');
         echo '<b>ChatGPT says:</b> <i>' . $fortune . '</i><br />';
         flush();
         $iteration = 0;
       }
+*/
       $updating = intval(trim(shell_exec('/usr/local/bin/nems-info quickfix')));
     }
     echo 'NEMS Linux reports ready to proceed.<br />';
